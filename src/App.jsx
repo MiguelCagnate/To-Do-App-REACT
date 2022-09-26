72aa4d2d3948790b984c0868fe661ca0e885ab54
omikeflies <omikeflies@gmail.com>
2022-09-26 13:09:00 +0200

    avui

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,58 @@
-import React, { Fragment, useState, useRef } from "react";
-import { v4 as uuidV4 } from "uuid";
+import React, { Fragment, useState, useRef, useEffect } from "react";
+import {v4 as uuidv4} from "uuid"
 import { TodoList } from "./components/TodoList";
+
+const KEY = "todoApp.todos"
 
 
 export function App() {
   const [todos, setTodos] = useState([
     { id: 1, task: "Tarea One", completed: false },
   ]);
-const todoTaskRef = useRef()
+const todoTaskRef = useRef(); 
+
+useEffect(() => {
+localStorage.setItem(KEY,JSON.stringify(todos))
+}, [todos])
+
+useEffect(() => {
+const storedTodos = JSON.parse(localStorage.getItem(KEY));
+if (storedTodos) {
+  setTodos(storedTodos); 
+}
+}, []); 
+
+const toggleTodo = (id) =>{
+  const newTodos = [...todos]
+  const todo = newTodos.find((todo) => todo.id === id);
+  todo.completed = !todo.completed;
+  setTodos(newTodos); 
+}
+
 const addNewTodo = () =>{
 const task = todoTaskRef.current.value
 if (task === '')return;  
 setTodos((prevTodos) => {
-    return [...prevTodos, {id:uuidV4(), task, completed:false}]
-})
+    return [...prevTodos, {id: uuidv4(), task, completed:false}]
+});
+todoTaskRef.current.value = null
 
 }
 
+
+const handleClearAll = () => {
+const newTodos =  todos.filter((todo) => todo.completed);
+setTodos(newTodos); 
+}
+
+
   return (
     <Fragment>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} toggleTodo={toggleTodo}/>
       <input ref={todoTaskRef} type="text" placeholder="Nueva Tarea"></input>
       <button onClick={addNewTodo}>➕</button>
-      <button>🗑</button>
+      <button onClick={handleClearAll}>🗑</button>
+      <div>You have {todos.filter((todo) => !todo.completed).length} task left to finish</div>
     </Fragment>
   );
 }
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
-export function TodoItem({ todo }) {
-    const {id, task, completed} = todo
-  return <li>{task}</li>;
+export function TodoItem({ todo, toggleTodo }) {
+    const {id, task, completed} = todo; 
+
+    const handleTodoClick = () => {
+      toggleTodo(id); 
+    }
+
+  return <li>
+  <input type='checkbox' checked={completed}   onChange={handleTodoClick}/>
+  {task}
+  </li>;
 }
